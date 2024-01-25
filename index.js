695251a8385417cb4e92ea3bbc7270c3f381a146
DomenicoMereu97 <75221419+DomenicoMereu97@users.noreply.github.com>
2024-01-25 12:43:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/admin', (req, res) => {
     res.sendFile(__dirname + '/admin.html');
 });
 
-app.get('/', (req, res) => {
+app.get('/utente', (req, res) => {
     res.sendFile(__dirname + '/utente.html');
 });
 
